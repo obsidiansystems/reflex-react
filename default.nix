4c1b43b4bd5f83ea44bd6ed266535caea891c20f
cidkidnix <cidkidnix@protonmail.com>
2023-05-25 09:07:03 -0500

    Update reflex-platform

--- default.nix
+++ default.nix
@@ -6,9 +6,9 @@
   ghcjs-compiler-nix-name = "ghcjs8107JSString"; #TODO: This must be default
   compiler-nix-name = "ghc8107Splices"; #TODO: This must be default
   shells = ps: with ps; [ reflex-react ];
-  inputMap = {
-    "https://github.com/obsidiansystems/react-haskell.git/44647cb28ab60872dc47ef1a9d38d02a1edadd61" = thunkSource ./dep/react-haskell;
-  };
+  inputMap = [
+    ./dep/react-haskell
+  ];
   overrides = [
     ({ config, pkgs, lib, ... }: {
       packages.react-haskell = {
