a8eac06c23517b4e717134e413b793445f3bb9a3
Divam <dfordivam@gmail.com>
2023-07-01 09:15:42 +0900

    fix lint error

--- example/components/Test.tsx
+++ example/components/Test.tsx
@@ -8,7 +8,7 @@ export default function Test() {
   return (
     <WithHaskell fallback={<i>Loading Haskell...</i>}>
       <button onClick={() => setV((old) => old+1)}>Increment</button>
-      <div id={v} />
+      <div id={v.toString()} />
       <ul>
         <li><Suspense fallback={<i>Loading Haskell...</i>}><HaskellComponent name="reflex" v={v} /></Suspense></li>
         <li><Suspense fallback={<i>Loading Haskell...</i>}><HaskellComponent name="reflex" v={v} /></Suspense></li>
