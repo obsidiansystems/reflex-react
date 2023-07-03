c39359d40605c632e240d29306a7cd9700ea57e5
Divam <dfordivam@gmail.com>
2023-07-03 14:05:03 +0900

    Fix for webpack errors with fs, child_process packages

--- example/next.config.js
+++ example/next.config.js
@@ -3,6 +3,11 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, options) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      child_process: false
+    };
     config.module.rules.push({
       test: /\.cabal/,
       use: [
